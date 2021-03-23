a3a60fcb7c9e560ef40d46d7358284cc03655e96
magicKen <i@magicken.com>
2021-03-23 20:19:51 +0800

    first commit

---
+++ HelloCompose/app/src/main/java/com/magicken/hellocompose/MainActivity.kt
@@ -0,0 +1,70 @@
+package com.magicken.hellocompose
+
+import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+class MainActivity : AppCompatActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContent {
+      NewsStory()
+    }
+  }
+
+  @Composable
+  fun NewsStory() {
+    MaterialTheme {
+      val typography = MaterialTheme.typography
+      Column(
+        modifier = Modifier.padding(16.dp)
+      ) {
+        Image(
+          painter = painterResource(id = R.drawable.header),
+          contentDescription = "header",
+          modifier = Modifier
+            .height(180.dp)
+            .fillMaxWidth()
+            .clip(RoundedCornerShape(4.dp)),
+          contentScale = ContentScale.Crop
+        )
+        Spacer(Modifier.requiredHeight(16.dp))
+        Text(
+          "A day wandering through the sandhills " +
+            "in Shark Fin Cove, and a few of the " +
+            "sights I saw",
+          style = typography.h6,
+          maxLines = 2,
+          overflow = TextOverflow.Ellipsis,
+        )
+        Text(
+          "Davenport, California",
+          style = typography.body2
+        )
+        Text(
+          "December 2018",
+          style = typography.body1
+        )
+      }
+    }
+  }
+
+  @Preview
+  @Composable
+  fun PreviewGreeting() {
+    NewsStory()
+  }
+}
