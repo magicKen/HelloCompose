cd95f8bc026b157261f7589350956b4a7dbd1834
magicKen <i@magicken.com>
2021-04-07 14:52:12 +0800

    feat: 将layout分到不同文件中

--- Layouts/app/src/main/java/com/magicken/layouts/MainActivity.kt
+++ Layouts/app/src/main/java/com/magicken/layouts/MainActivity.kt
@@ -3,29 +3,6 @@ package com.magicken.layouts
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Face
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.magicken.layouts.ui.ImageList
 import com.magicken.layouts.ui.theme.LayoutsTheme
 
@@ -37,43 +14,5 @@ class MainActivity : ComponentActivity() {
         ImageList()
       }
     }
-  }
-}
-
-@Composable
-fun PhotographerCard(modifier: Modifier = Modifier) {
-  Row(
-    modifier
-      .padding(8.dp)
-      .clip(RoundedCornerShape(4.dp))
-      .background(MaterialTheme.colors.surface)
-      .clickable { }
-      .padding(16.dp)
-  ) {
-    Surface(
-      modifier = Modifier.size(50.dp),
-      shape = CircleShape,
-      color = MaterialTheme.colors.onSurface.copy(alpha = 0.2f)
-    ) {
-
-    }
-    Column(
-      modifier = Modifier
-        .padding(start = 8.dp)
-        .align(Alignment.CenterVertically)
-    ) {
-      Text("Alfred Sisley", fontWeight = FontWeight.Bold)
-      CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
-        Text("3 minutes ago", style = MaterialTheme.typography.body2)
-      }
-    }
-  }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun PhotographerCardPreview() {
-  LayoutsTheme {
-    PhotographerCard()
   }
 }
